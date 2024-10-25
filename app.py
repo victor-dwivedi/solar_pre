3b1220a387079b5d020335bdd7f6c12cb2fd271f
RISHIKESH DWIVEDI <129958222+victor-dwivedi@users.noreply.github.com>
2024-10-25 18:43:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,8 @@ def predict_solar_energy(model, sunlight_hours, cloud_cover, temperature):
 def main():
     st.title("Solar Energy Prediction App")
 
-    API_KEY = "6905b6c823214623958124021242510"  # Replace with your actual Weather API key
+    API_KEY = "15126be931c44b49917131244242510"  # Replace with your actual Weather API key
+    
     LOCATION = st.text_input("Enter Location:", value="Nagpur")
     
     if st.button("Fetch Weather Data"):
